c7163c78897cdebf04116922bc2afb4c96ba1127
Romina Aguado <aguado.romi@gmail.com>
2021-05-22 17:24:32 -0500

    Building header

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
 <?php get_header(); ?>
 
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/img/blackDog.jpg') ?>);"></div>
+<div class="header">
+<div  class="header__background" style="background-image: url(<?php echo get_theme_file_uri('/img/blackDog.jpg') ?>);"></div>
 
-
-
-
-      <header class="header">
+<div class="header__logo-box">
+        
+      </div>
       <div class="header__text-box">
         <h1 class="heading-primary">
           <span class="heading-primary--main">Hund Svart</span>
           <span class="heading-primary--sub">Online solutions to boost your business</span>
         </h1>
-        <a href="#section-tours" class="btn btn--white btn--animated">Discover our work</a>
+        <a href="#" class="btn btn--white btn--animated">Contact Us!</a>
       </div>
-    </header>
-   
-    </div>
-  </div>
+</div>
